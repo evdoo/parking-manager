673e824f3c46b128c8b6fc885e8afa3a7396b185
Olga Evdokimova <o.evdokima@gmail.com>
2023-02-21 19:29:31 +0300

    Код переделан на более лаконичный

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,23 +1,31 @@
 fun main(args: Array<String>) {
 
     while (true) {
-        if (readLine().equals("/start")) {
-            println("Здравствуйте! Введите команду:")
-            break
-        } else {
-            println("Неверный формат данных, попробуйте еще раз.")
+        when (readLine()) {
+            "/start" -> {
+                println("Здравствуйте! Введите команду:")
+                break
+            }
+            else -> println("Неверный формат данных, попробуйте еще раз.")
         }
     }
 
     while (true) {
-        val input = readLine()
-        if (input.equals("/help")) {
-            println("Доступные команды:\n/start - начало работы.\n/help - вызов справки по командам.\n/end - завершение работы и выход из программы.")
-        } else if (input.equals("/end")) {
-            println("До свидания. Хорошего дня")
-            break
-        } else {
-            println("Неверный формат данных, попробуйте еще раз.\nДля вызова справки используйте команду /help")
+        when (readLine()) {
+            "/help" -> {
+                println("Доступные команды:")
+                println("/start - начало работы.")
+                println("/help - вызов справки по командам.")
+                println("/end - завершение работы и выход из программы.")
+            }
+            "/end" -> {
+                println("До свидания. Хорошего дня")
+                break
+            }
+            else -> {
+                println("Неверный формат данных, попробуйте еще раз.")
+                println("Для вызова справки используйте команду / help")
+            }
         }
     }
 }
